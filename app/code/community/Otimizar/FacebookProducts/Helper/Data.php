493ef9a14fa8d9f95a0c766d5a04685419bcde19
arioch1984 <arioch1984@gmail.com>
2016-03-15 10:10:30 +0100

    helper for debug mode

--- app/code/community/Otimizar/FacebookProducts/Helper/Data.php
+++ app/code/community/Otimizar/FacebookProducts/Helper/Data.php
@@ -1,4 +1,10 @@
 <?php
 class Otimizar_FacebookProducts_Helper_Data extends Mage_Core_Helper_Abstract {
 
+	const IS_DEBUG_MODE_ON = 'dev/fb_product_feed/check_is_debug_enabled';
+
+	public function isDebugModeOn() {
+		return Mage::getStoreConfig(self::IS_DEBUG_MODE_ON);
+	}
+
 }
