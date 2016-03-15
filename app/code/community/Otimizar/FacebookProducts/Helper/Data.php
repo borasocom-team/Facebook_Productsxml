4cc3d82d52f4638ed02e0f5d95447e5b928e2d95
arioch1984 <arioch1984@gmail.com>
2016-03-15 11:09:44 +0100

    helper for log file

--- app/code/community/Otimizar/FacebookProducts/Helper/Data.php
+++ app/code/community/Otimizar/FacebookProducts/Helper/Data.php
@@ -2,10 +2,11 @@
 class Otimizar_FacebookProducts_Helper_Data extends Mage_Core_Helper_Abstract {
 
 	const IS_DEBUG_MODE_ON = 'dev/fb_product_feed/check_is_debug_enabled';
+	const LOG_FILE_NAME = 'fb_product_feed_module.log';
 
-	public function getLogFileName() {
-		$fbLogFileName = Mage::getBaseDir().DS.'var'.DS.'log'.DS.'fb_product_feed_module.log';
-		return $fbLogFileName;
+	public function getLogFileFullPath() {
+		$fbLogFileFullPath = Mage::getBaseDir().DS.'var'.DS.'log'.DS.'fb_product_feed_module.log';
+		return $fbLogFileFullPath;
 	}
 
 	public function isDebugModeOn() {
@@ -14,15 +15,15 @@ class Otimizar_FacebookProducts_Helper_Data extends Mage_Core_Helper_Abstract {
 
 	public function emptyLogFile() {
 		$fileObject = new Varien_Io_File();
-		if($fileObject->fileExists($this->getLogFileName())) {
-			if($fileObject->isWriteable($this->getLogFileName())) {
-				$fileObject->rm($this->getLogFileName());
+		if($fileObject->fileExists($this->getLogFileFullPath())) {
+			if($fileObject->isWriteable($this->getLogFileFullPath())) {
+				$fileObject->rm($this->getLogFileFullPath());
 			}
 		}
 	}
 
 	public function writeLogFile($value, $level = 0) {
-		Mage::log($value,$level,$this->getLogFileName());
+		Mage::log($value,$level,self::LOG_FILE_NAME);
 	}
 
 }
