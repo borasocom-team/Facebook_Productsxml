c0af8cf4c9124caccba59c81456a9b59e365e921
arioch1984 <arioch1984@gmail.com>
2016-03-15 10:53:17 +0100

    helper for log file

--- app/code/community/Otimizar/FacebookProducts/Helper/Data.php
+++ app/code/community/Otimizar/FacebookProducts/Helper/Data.php
@@ -3,8 +3,26 @@ class Otimizar_FacebookProducts_Helper_Data extends Mage_Core_Helper_Abstract {
 
 	const IS_DEBUG_MODE_ON = 'dev/fb_product_feed/check_is_debug_enabled';
 
+	public function getLogFileName() {
+		$fbLogFileName = Mage::getBaseDir().DS.'var'.DS.'log'.DS.'fb_product_feed_module.log';
+		return $fbLogFileName;
+	}
+
 	public function isDebugModeOn() {
 		return Mage::getStoreConfig(self::IS_DEBUG_MODE_ON);
+	}
+
+	public function emptyLogFile() {
+		$fileObject = new Varien_Io_File();
+		if($fileObject->fileExists($this->getLogFileName())) {
+			if($fileObject->isWriteable($this->getLogFileName())) {
+				$fileObject->rm($this->getLogFileName());
+			}
+		}
+	}
+
+	public function writeLogFile($value, $level = 0) {
+		Mage::log($value,$level,$this->getLogFileName());
 	}
 
 }
